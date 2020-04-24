6ac0b1d763942790cdd68dccf13f9da1a8b319ec
John <omnisonic@gmail.com>
2020-04-24 11:49:08 -0700

    added request.html to page templates

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class DonationRequest(models.Model):
+    username = models.CharField(max_length=30)
+    text = models.CharField(max_length=160)
+    created = models.DateTimeField(auto_now_add=True)
